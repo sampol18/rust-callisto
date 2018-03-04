88b877412dbd67afce32f027a73f1be0d08eaea0
Yohan Graterol <yohangraterol92@gmail.com>
2018-03-04 16:40:28 -0500

    Add CLO reward

--- ethcore/src/ethereum/ethash.rs
+++ ethcore/src/ethereum/ethash.rs
@@ -267,6 +267,13 @@ impl Engine<EthereumMachine> for Arc<Ethash> {
 		let eras_rounds = self.ethash_params.ecip1017_era_rounds;
 		let (eras, reward) = ecip1017_eras_block_reward(eras_rounds, reward, number);
 
+		// Applies Callisto reward.
+		let reward = if number >= self.ethash_params.callisto_transition {
+			self.ethash_params.callisto_miner_reward
+		} else {
+			reward
+		};
+
 		let n_uncles = LiveBlock::uncles(&*block).len();
 
 		// Bestow block rewards.
@@ -284,7 +291,6 @@ impl Engine<EthereumMachine> for Arc<Ethash> {
 			self.machine.add_balance(block, &ubi_contract, &ubi_reward)?;
 			self.machine.add_balance(block, &dev_contract, &dev_reward)?;
 		} else if number >= self.ethash_params.callisto_transition {
-			result_block_reward = self.ethash_params.callisto_miner_reward;
 			let treasury_address = self.ethash_params.callisto_treasury_address;
 			let treasury_reward = self.ethash_params.callisto_treasury_reward;
 			let stake_address = self.ethash_params.callisto_stake_address;
